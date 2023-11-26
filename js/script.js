e51ef8cd4fb8574fd304da100fd24ab8e3bbbc4a
sunnercom <nuriddinovsunnatbek9@gmail.com>
2023-11-26 09:37:27 +0200

    .

--- js/script.js
+++ js/script.js
@@ -1,28 +1,28 @@
 var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 4,
-    navigation: {
-      nextEl: ".next-one",
-      prevEl: ".prev-one",
+  slidesPerView: 4,
+  navigation: {
+    nextEl: ".next-one",
+    prevEl: ".prev-one",
+  },
+  loop: true,
+  breakpoints: {
+    992: {
+      slidesPerView: 3,
+      spaceBetween: 30,
     },
-    loop: true,
-    breakpoints: {
-      992: {
-        slidesPerView: 3,
-        spaceBetween: 30,
-      },
-      760: {
-        slidesPerView: 2.5,
-        spaceBetween: 20,
-      },
-      630: {
-        slidesPerView: 2,
-      },
-      480: {
-        slidesPerView: 1.5,
-      },
-      320: {
-        slidesPerView: 1,
-        spaceBetween: 15,
-      }
+    760: {
+      slidesPerView: 2.5,
+      spaceBetween: 20,
+    },
+    630: {
+      slidesPerView: 2,
+    },
+    480: {
+      slidesPerView: 1.5,
+    },
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 15,
     }
-  });
+  }
+});
