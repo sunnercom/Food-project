ff5465c0a9dc509a753d66d370340aa99b10b80b
sunnercom <nuriddinovsunnatbek9@gmail.com>
2023-11-26 23:36:29 +0200

    contact us-page finished

--- js/script.js
+++ js/script.js
@@ -26,3 +26,20 @@ var swiper = new Swiper(".mySwiper", {
     }
   }
 });
+
+function toggleAccordion(sectionNumber) {
+  var accordionContent = document.querySelectorAll('.accordion-content');
+  var currentAccordionContent = accordionContent[sectionNumber - 1];
+
+  // Barcha accordion-content'larini yopish
+  for (var i = 0; i < accordionContent.length; i++) {
+    accordionContent[i].style.display = 'none';
+  }
+
+  // Bosilgan accordion-content'ni ochish yoki yopish
+  if (currentAccordionContent.style.display === 'block') {
+    currentAccordionContent.style.display = 'none';
+  } else {
+    currentAccordionContent.style.display = 'block';
+  }
+}
